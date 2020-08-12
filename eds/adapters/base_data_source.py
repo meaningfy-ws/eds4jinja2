d34c5b8ce43464c0be2c508489240a46540bab80
lps <costezki.eugen@gmail.com>
2020-08-12 13:58:49 +0200

    refactor the base DataSource class

--- eds/adapters/base_data_source.py
+++ eds/adapters/base_data_source.py
@@ -11,17 +11,6 @@ from abc import ABC, abstractmethod
 from typing import Tuple, Optional
 
 
-class Representation(enum.Enum):
-    TABULAR = 1
-    TREE = 2
-
-
-class UnknownRepresentation(Exception):
-    """
-        Unknown representation exception
-    """
-
-
 class UnsupportedRepresentation(Exception):
     """
         Unsupported representation exception
@@ -33,31 +22,33 @@ class DataSource(ABC):
     generic data source
     """
 
-    def fetch(self, representation: Representation) -> Tuple[object, Optional[str]]:
+    def fetch_tabular(self) -> Tuple[Optional[object], Optional[str]]:
         """
-            read the content from the data source in either tree or tabular structure
-        :type representation: specifies a representation of the read data: as tabular or tree
+            read the content from the data source in tabular structure
         :return: a tuple where the first element is a result of a successful data reading and
                  the second is the error message in case of failure
         """
-        if representation is Representation.TREE:
-            return self._fetch_tree()
-        elif representation is Representation.TABULAR:
-            return self._fetch_tabular()
-        else:
-            raise UnknownRepresentation(f"The representation {str(representation)} is not supported")
+        try:
+            if not self._can_be_tabular():
+                raise UnsupportedRepresentation("Only TREE representation is supported")
+            else:
+                return self._fetch_tabular(), None
+        except Exception as e:
+            return None, str(e)
 
-    def fetch_tabular(self) -> Tuple[object, Optional[str]]:
-        if not self._can_be_tabular():
-            raise UnsupportedRepresentation("Only TREE representation is supported")
-        else:
-            return self._fetch_tabular()
-
-    def fetch_tree(self) -> Tuple[object, Optional[str]]:
-        if not self._can_be_tree():
-            raise UnsupportedRepresentation("Only TABULAR representation is supported")
-        else:
-            return self._fetch_tree()
+    def fetch_tree(self) -> Tuple[Optional[object], Optional[str]]:
+        """
+        read the content from the data source in tree structure
+        :return: a tuple where the first element is a result of a successful data reading and
+                 the second is the error message in case of failure
+        """
+        try:
+            if not self._can_be_tree():
+                raise UnsupportedRepresentation("Only TABULAR representation is supported")
+            else:
+                return self._fetch_tree(), None
+        except Exception as e:
+            return None, str(e)
 
     @abstractmethod
     def _can_be_tabular(self) -> bool:
@@ -68,7 +59,7 @@ class DataSource(ABC):
         pass
 
     @abstractmethod
-    def _fetch_tabular(self) -> Tuple[object, Optional[str]]:
+    def _fetch_tabular(self):
         """
             fetch data and return as tabular representation
         :return:
@@ -76,7 +67,7 @@ class DataSource(ABC):
         pass
 
     @abstractmethod
-    def _fetch_tree(self) -> Tuple[object, Optional[str]]:
+    def _fetch_tree(self):
         """
             fetch data and return as tree representation
         :return:
