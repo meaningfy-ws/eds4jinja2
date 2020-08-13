c283293d69a267e8285581d99159e4357b91e733
lps <costezki.eugen@gmail.com>
2020-08-13 21:55:17 +0200

    upgrade sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -28,12 +28,13 @@ author = 'Eugeniu Costetchi'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser",
-              "sphinxcontrib.apidoc",
-              "sphinx.ext.autodoc",
-              "sphinx.ext.intersphinx",
-              "sphinx.ext.viewcode",
-              ]
+extensions = [
+    # "myst_parser",
+    "sphinxcontrib.apidoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+]
 
 apidoc_module_dir = "../eds4jinja2"
 apidoc_output_dir = "srcdocs"
