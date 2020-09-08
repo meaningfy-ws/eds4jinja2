0ab775d86bb9cfcdcab7e0752970bb6e761f07e2
Laurentiu Mandru <mclaurentiu79@gmail.com>
2020-09-08 23:48:53 +0300

    Added builder functionality

--- eds4jinja2/builders/report_builder.py
+++ eds4jinja2/builders/report_builder.py
@@ -2,8 +2,8 @@
 
 # report_generator
 # Created:  08/03/2019
-# Author: Eugeniu Costetchi
-# Email: costezki.eugen@gmail.com
+# Author: Laurentiu Mandru
+# Email: mclaurentiu79@gmail.com
 
 """
 this module implements the report generation functionality.
@@ -24,40 +24,51 @@ class ReportBuilder:
     __STATIC_FOLDER__ = "static"
     __TEMPLATE_FOLDER__ = "templates"
     __DEFAULT_CONFIG_FILE__ = "config.json"
-    __DEFAULT_OUTPUT_DIRECTORY__ = "output"
+    __DEFAULT_OUTPUT_FOLDER__ = "output"
 
-    def __init__(self, input_folder, config_file=__DEFAULT_CONFIG_FILE__, output_folder=__DEFAULT_OUTPUT_DIRECTORY__):
+    def __init__(self, target_path, config_file=__DEFAULT_CONFIG_FILE__, output_path=None):
         """
             Instantiates builders form a template providing an optional configuration context.
 
-        :param template_folder: the folder where JINJA2 templates are found
-        :param main_template_name: the main builders template that will be used to generate the builders
-        :param configuration_context: the configuration context available in the templates under the key "configuration"
+        :param target_path: the folder where the required resources are found
+        :param config_file: the configuration file (default 'config.json')
+        :param output_path: the output folder where the result of the rendering will be created
         """
 
-        with open(pathlib.Path(input_folder) / config_file, encoding='utf-8') as configFile:
+        with open(pathlib.Path(target_path) / config_file, encoding='utf-8') as configFile:
             configuration_context = json.loads(configFile.read())
 
         self.template = configuration_context["template"]
         self.configuration_context = configuration_context
-        self.__DEFAULT_OUTPUT_DIRECTORY__ = output_folder
 
-        template_loader = jinja2.FileSystemLoader(searchpath=str(pathlib.Path(input_folder) / self.__TEMPLATE_FOLDER__))
+        if output_path is not None:
+            self.__DEFAULT_OUTPUT_FOLDER__ = pathlib.Path(output_path) / self.__DEFAULT_OUTPUT_FOLDER__
+
+        self.__STATIC_FOLDER__ = pathlib.Path(target_path) / self.__STATIC_FOLDER__
+
+        template_loader = jinja2.FileSystemLoader(searchpath=str(pathlib.Path(target_path) / self.__TEMPLATE_FOLDER__))
         self.template_env = build_eds_environment(loader=template_loader)
         inject_environment_globals(self.template_env, {'conf': self.configuration_context["conf"]},
                                    False if configuration_context is None else True)
-        self.__beforeRenderingListeners = []
-        self.__afterRenderingListeners = []
+        self.__before_rendering_listeners = []
+        self.__after_rendering_listeners = []
 
-    def addBeforeRenderingListener(self, listener):
-        self.__beforeRenderingListeners.append(listener)
+    def add_before_rendering_listener(self, listener):
+        """
+            At the moment there is no use for this.
+        """
+        self.__before_rendering_listeners.append(listener)
 
-    def addAfterRenderingListener(self, listener):
-        self.__afterRenderingListeners.append(listener)
+    def add_after_rendering_listener(self, listener):
+        """
+            At the moment, this is used just to copy static files to the output directory.
+            For example, CSS, JavaScript and other static resources.
+        """
+        self.__after_rendering_listeners.append(listener)
 
     def __get_template(self, template_name=None) -> jinja2.Template:
         """
-            Standard JINA2 get_template() functionality.
+            Standard JINJA2 get_template() functionality.
         :param template_name:
         :return:
         """
@@ -66,23 +77,14 @@ class ReportBuilder:
         else:
             return self.template_env.get_template(template_name)
 
-    # def __from_string(self, template_string) -> jinja2.Template:
-    #     """
-    #         Standard JINA2 from_string() functionality.
-    #     :param template_string:
-    #     :return:
-    #     """
-    #     self.template_env.from_string(template_string, globals=self.template_env.globals)
-
     def make_document(self):
-        for listener in self.__beforeRenderingListeners:
+        for listener in self.__before_rendering_listeners:
             listener()
 
         template = self.__get_template(self.template)
 
-        template.stream().dump("output.html")
-        # copy resources - perhaps this is best to be done by an after rendering method that should be registered
-        # by the consumer of this class
+        pathlib.Path(self.__DEFAULT_OUTPUT_FOLDER__).mkdir(parents=True, exist_ok=True)
+        template.stream().dump(str(pathlib.Path(self.__DEFAULT_OUTPUT_FOLDER__) / self.template))
 
-        for listener in self.__afterRenderingListeners:
-            listener()
+        for listener in self.__after_rendering_listeners:
+            listener(str(self.__STATIC_FOLDER__), str(pathlib.Path(self.__DEFAULT_OUTPUT_FOLDER__)))
--- eds4jinja2/main.py
+++ eds4jinja2/main.py
@@ -1,5 +1,22 @@
+from distutils.dir_util import copy_tree
+
 from eds4jinja2.builders.report_builder import ReportBuilder
+import click
+
+
+def copy_static_content(from_path, to_path):
+    copy_tree(from_path,to_path)
+
+
+@click.command()
+@click.option("--target", required=True, type=str, help="The folder containing the config.json")
+@click.option("--config", default="config.json", type=str, help="The configuration file if named other than config.json")
+@click.option("--output", required=False, type=str, help="The output folder")
+def build_report(target, config, output):
+    report_builder = ReportBuilder(target, config, output)
+    report_builder.add_after_rendering_listener(copy_static_content)
+    report_builder.make_document()
+
 
 if __name__ == "__main__":
-    repBuilder = ReportBuilder("/home/laur/work/eds4jinja2/tests/test_data/templates_test")
-    repBuilder.make_document()
+    build_report()
