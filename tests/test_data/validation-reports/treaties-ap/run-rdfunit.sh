65558d2a5c26f6fb043cceb2da60b195db8a34c5
lps <costezki.eugen@gmail.com>
2020-10-05 19:35:25 +0200

    update template add simple validation data

--- tests/test_data/validation-reports/treaties-ap/run-rdfunit.sh
+++ tests/test_data/validation-reports/treaties-ap/run-rdfunit.sh
@@ -2,11 +2,12 @@
 
 DATA_FOLDER=`pwd`
 CONTAINER_DATA_FOLDER=/mnt/data
-DATA_FILE=$CONTAINER_DATA_FOLDER/treaties-2019-01-28-nal-src-ap.rdf
+#DATA_FILE=$CONTAINER_DATA_FOLDER/treaties-2019-01-28-nal-src-ap.rdf
+DATA_FILE=$CONTAINER_DATA_FOLDER/treaties-2019-01-28-nal-src-ap.ttl
 #DATA_FILE=$CONTAINER_DATA_FOLDER/report-itb-treaties-skos-ap.ttl
 SHAPE_FILE=$CONTAINER_DATA_FOLDER/skosShapes.shapes.ttl
 
 
-#docker run -v $DATA_FOLDER:${CONTAINER_DATA_FOLDER}  --rm -it aksw/rdfunit -d $DATA_FILE -u $DATA_FILE  -s $SHAPE_FILE -r shacl -o html,ttl -f $CONTAINER_DATA_FOLDER/
+docker run -v $DATA_FOLDER:${CONTAINER_DATA_FOLDER}  --rm -it aksw/rdfunit -d $DATA_FILE -u $DATA_FILE  -s $SHAPE_FILE -r shacl -o html,ttl -f $CONTAINER_DATA_FOLDER/
 
-docker run -v $DATA_FOLDER:$CONTAINER_DATA_FOLDER  --rm -it  --entrypoint bash aksw/rdfunit
+#docker run -v $DATA_FOLDER:$CONTAINER_DATA_FOLDER  --rm -it  --entrypoint bash aksw/rdfunit
