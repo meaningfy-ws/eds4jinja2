925aa1b409ee85ac7859a2ab652b39d894a759e4
lps <costezki.eugen@gmail.com>
2020-10-08 10:05:48 +0200

    rename the template folder update the setup.py add MANIFEST.in

---
+++ rdf_report_templates/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# __init__.py
+# Date:  08/10/2020
+# Author: Eugeniu Costetchi
+# Email: costezki.eugen@gmail.com 
+
+""" This package includes eds4jinja2 templates used by a few Meaningfy projects """
