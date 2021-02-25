49e1cf6198177c9aad29347e486526410eaad1ba
lps <costezki.eugen@gmail.com>
2021-02-25 23:21:15 +0100

    add connection pool to the remote_sparql_ds.py SPARQL adapter

--- eds4jinja2/__init__.py
+++ eds4jinja2/__init__.py
@@ -9,7 +9,7 @@ __docformat__ = "restructuredtext en"
 
 # The format of the __version__ line is matched by a regex in setup.py and /docs/conf.py
 __version__ = "0.1.32"
-__date__ = "2021-02-23"
+__date__ = "2021-02-25"
 
 import logging
 
