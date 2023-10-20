8ad4875a191e550fac58d072e75a23272923404e
Rashif Ray Rahman <schiv@archlinux.org>
2023-10-20 22:12:17 +0600

    New version 0.2.0, dropping support for Python <= 3.7
    Python 3.7 and earlier reached EOL earlier this year, and many
    tools/libraries have stopped supporting those runtime. Continuing to
    support an old runtime will be near impossible due to dependency hell.
    It is therefore only wise to follow suit.
    
    - Update code, tests
    - Update CI config
    - Update dependencies
    - Update metadata
    
    We will aim to support and test against the oldest `security` release
    and the latest `bugfix` release, less one (if it's too new and not a lot
    of tools have adopted it).
    
    The jump from Python 3.6 to 3.8 doesn't involve that many changes among
    dependency APIs for this project -- there is just a
    `collections.Mapping` namespace change and a Pandas
    `DataFrame.replace()` field behaviour change.
    
    Users are encouraged to upgrade even if there is no API or ABI change in
    this project itself, as an EOL runtime is a security risk.

--- eds4jinja2/__init__.py
+++ eds4jinja2/__init__.py
@@ -8,8 +8,8 @@
 __docformat__ = "restructuredtext en"
 
 # The format of the __version__ line is matched by a regex in setup.py and /docs/conf.py
-__version__ = "0.1.36"
-__date__ = "2022-05-06"
+__version__ = "0.2.0"
+__date__ = "2023-10-20"
 
 import logging
 
